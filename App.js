abee9bc606e6252683c5f3a1815314f7e2b86e99
MuChuan Hung <61078091+muchuanhung@users.noreply.github.com>
2023-09-12 11:02:44 +0800

    feat: install Eslint & prettier & react native paper

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar as ExpoStatusbar } from "expo-status-bar";
+import { StatusBar, StyleSheet, SafeAreaView, Text, View} from "react-native";
+import { Searchbar } from 'react-native-paper';
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = React.useState('');
+  const onChangeSearch = query => setSearchQuery(query);
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // 判斷針對不同的Platform做layout的調整
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Searchbar
+            placeholder="Search"
+            onChangeText={onChangeSearch}
+            value={searchQuery}
+          />
+        </View>
+        <View style={styles.list}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusbar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    flex: 1,
+    backgroundColor: "green",
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    backgroundColor: "orange",
+    padding: 16,
   },
 });
