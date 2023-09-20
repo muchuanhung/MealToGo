501a1caee8621c4393b2be88a7859babfb60575b
MuChuan Hung <61078091+muchuanhung@users.noreply.github.com>
2023-09-20 16:57:43 +0800

    feat: tab navigation template

--- test.js
+++ test.js
@@ -5,16 +5,16 @@ import { ThemeProvider } from "styled-components/native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import { SafeArea } from "./src/utils/safe-area.component";
-import { RestaurantsScreen } from "./src/features/restaurants/components/screens/restaurants.screen";
-//import style component setting
-import { theme } from "./src/infrastructure/theme";
-
-// import custom font from google font
-import { useFonts as useOswald, Oswald_400Regular } from "@expo-google-fonts/oswald";
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
-// 宣告bottom tab section value
+import { theme } from "./src/infrastructure/theme";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { SafeArea } from "./src/components/utility/safe-area.component";
+
 const Tab = createBottomTabNavigator();
 
 const Settings = () => (
@@ -27,14 +27,8 @@ const Map = () => (
     <Text>Map</Text>
   </SafeArea>
 );
-const Other = () => (
-  <SafeArea>
-    <Text>Other</Text>
-  </SafeArea>
-);
 
-// Define custom font
- export default function App() {
+export default function App() {
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
@@ -48,16 +42,13 @@ const Other = () => (
   }
 
   return (
-    // 判斷針對不同的Platform做layout的調整
-    // build different bottom tab section
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
           <Tab.Navigator>
-              <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
-              <Tab.Screen name="Map" component={Map} />
-              <Tab.Screen name="Settings" component={Settings} />
-              <Tab.Screen name="Other" component={Other} />
+            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+            <Tab.Screen name="Map" component={Map} />
+            <Tab.Screen name="Settings" component={Settings} />
           </Tab.Navigator>
         </NavigationContainer>
       </ThemeProvider>
